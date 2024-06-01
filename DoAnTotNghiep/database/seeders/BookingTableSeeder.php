9821428db22d8d86d16cde626ff3e9dfb7d71720
TranTrongNghia1810 <99171991+TranTrongNghia1810@users.noreply.github.com>
2024-06-01 15:40:25 +0700

    create file controller

--- DoAnTotNghiep/database/seeders/BookingTableSeeder.php
+++ DoAnTotNghiep/database/seeders/BookingTableSeeder.php
@@ -4,6 +4,8 @@ namespace Database\Seeders;
 
 use Illuminate\Database\Console\Seeds\WithoutModelEvents;
 use Illuminate\Database\Seeder;
+use Illuminate\Support\Carbon;
+use Illuminate\Support\Facades\DB;
 
 class BookingTableSeeder extends Seeder
 {
@@ -12,6 +14,21 @@ class BookingTableSeeder extends Seeder
      */
     public function run(): void
     {
-        //
+        DB::table('booking')->insert([
+            [
+                'tour_id' => 2,
+                'customer_id' => 1,
+                'booking_date' => Carbon::now()->toDateString(),
+                'created_at' => Carbon::now(),
+                'updated_at' => Carbon::now(),
+            ],
+            [
+                'tour_id' => 3,
+                'customer_id' => 3,
+                'booking_date' => Carbon::now()->toDateString(),
+                'created_at' => Carbon::now(),
+                'updated_at' => Carbon::now(),
+            ],
+        ]);
     }
 }
